a7b40739dd40439e5608906e61054cf0ef8b709d
Matt Cole <mcole.uk@gmail.com>
2023-09-17 09:06:29 +0100

    Added Speed restriction Functionality

--- functions/configuration/express.js
+++ functions/configuration/express.js
@@ -19,6 +19,8 @@ module.exports = () => {
     require('../routes/accessRequest')(app);
     // the disruptive routes
     require('../routes/disruptive')(app);
+    // the speed restrictions routes
+    require('../routes/speedRestrictions')(app);
 
     return app;
 }
